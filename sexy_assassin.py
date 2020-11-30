ef7b2a542222b12a743b222ad15c95818ca54bf5
tecnosam <ikabolo59@gmail.com>
2020-11-30 20:26:19 +0200

    added exception handling for the sexy assasin

--- sexy_assassin.py
+++ sexy_assassin.py
@@ -5,21 +5,27 @@ from ftpclient import FileStorage
 
 class GracefulKiller:
   kill_now = False
-  fs = FileStorage( persist = True )
-  fs.restore()
-  fs.kill()
+  try:
+    fs = FileStorage( persist = True )
+    fs.restore()
+    fs.kill()
+  except:
+    print( "### Could not restore backup FTP server acting like a bitch again" )
   def __init__(self):
     signal.signal(signal.SIGINT, self.exit_gracefully)
     signal.signal(signal.SIGTERM, self.exit_gracefully)
 
   def exit_gracefully(self,signum, frame):
     self.kill_now = True
-    fs = FileStorage( persist = True )
-    print("Backing up data...")
-    fs.backup(  )
-    fs.kill()
-    print("Backup complete\nExiting...")
-    quit()
+    try:
+      fs = FileStorage( persist = True )
+      print("Backing up data...")
+      fs.backup(  )
+      fs.kill()
+      print("Backup complete\nExiting...")
+      quit()
+    except:
+      print("### Could not backup data. FTP server acting up again")
     # sys.exit( 0 )
 
 # if __name__ == '__main__':
